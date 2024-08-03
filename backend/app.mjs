66b38a83bb9da1bcdc3b72925f966fd24e756188
Yurkmez <yurkmez.shaikh@gmail.com>
2024-08-03 13:34:43 +0300

    (2) commit - organized interconnection between b & f (fetch, cors), transmitted and recived object (as example)

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 // import qs from 'querystring';
 const app = express();
 const PORT = 5000;
 
+// ________ MIDDLEWARE _________________________________
 // log info about request
 app.use(morgan('tiny'));
 // convert JSON object to usually JS object in POST, PUT, PATCH request
@@ -11,9 +13,15 @@ app.use(express.json());
 // convert form data to usually JS object in POST, PUT, PATCH request
 app.use(express.urlencoded({ extended: true }));
 // { extended: true } использовано qs, ктр есть в node_modules
+app.use(cors());
+// _________________________________________________________
 app.use((req, res) => {
-    console.log(req.body);
-    return res.send('This is express server');
+    const personData = {
+        name: 'I am',
+        exist: true,
+    };
+    // response JSON object
+    return res.json(personData);
 });
 
 app.listen(PORT, () => console.log(`Server is launched on port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [person, setPerson] = useState();
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setPerson(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+    return (
+        <div className="App">
+            {person && (
+                <>
+                    <h1>Who are you? - {person.name}</h1>
+                    <h2>This is true? - {person.exist.toString()}</h2>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
